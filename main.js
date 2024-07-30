674913db89a18dfa72a8cae601080b558b2cb659
ArthurTheDog0101 <129325489+ArthurTheDog0101@users.noreply.github.com>
2024-07-30 07:45:22 -0300

    senha alterar valor

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ const botoes = document.querySelectorAll('.parametro-senha__botao');
 console.log(botoes)
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
-       // tamanhoSenha = tamanhoSenha+1;
+         tamanhoSenha = tamanhoSenha+1;
        tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
 }
 function diminuiTamanho(){
     if (tamanhoSenha > 1){
-       // tamanhoSenha = tamanhoSenha-1;
+          tamanhoSenha = tamanhoSenha-1;
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
