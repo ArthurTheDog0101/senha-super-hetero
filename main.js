b9102c298e571d7ff0cfc5dc726f76d04ec43d7b
ArthurTheDog0101 <129325489+ArthurTheDog0101@users.noreply.github.com>
2024-07-30 07:54:33 -0300

    botao mudar teste 2

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
+botoes[0].onclick = diminuiTamanho;
+
 console.log(botoes)
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
