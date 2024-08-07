25e672697bb3494f126105811dc2b7eb16abe545
ArthurTheDog0101 <129325489+ArthurTheDog0101@users.noreply.github.com>
2024-08-07 09:35:17 -0300

    20

--- main.js
+++ main.js
@@ -9,13 +9,13 @@ botoes[0].onclick = diminuiTamanho;
 console.log(botoes)
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
-         tamanhoSenha = tamanhoSenha+0;
+         tamanhoSenha = tamanhoSenha+20;
        tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
 }
 function diminuiTamanho(){
-    if (tamanhoSenha > -10){
+    if (tamanhoSenha > 1){
           tamanhoSenha = tamanhoSenha-0;
         tamanhoSenha--;
     }
