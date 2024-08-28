a5ea2172e03ae75028a7e8d1b4f0c6b011521b86
ArthurTheDog0101 <129325489+ArthurTheDog0101@users.noreply.github.com>
2024-08-28 09:15:20 -0300

    aumenta tamanho senha consertado

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ numeroSenha.textContent = tamanhoSenha;
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
 
 console.log(botoes)
 function aumentaTamanho(){
-    if (tamanhoSenha < 1){
+    if (tamanhoSenha < 20){
          tamanhoSenha = tamanhoSenha+1;
        tamanhoSenha++;
     }
